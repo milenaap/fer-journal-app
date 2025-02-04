d42b0c3f3e1221eb9d9b53e263f9b1dd0453e195
mile <darimile@gmail.com>
2025-02-04 22:22:18 +0100

    add AuthLayout

--- src/modules/auth/pages/LoginPage.jsx
+++ src/modules/auth/pages/LoginPage.jsx
@@ -1,57 +1,52 @@
-import { Grid2, TextField, Typography } from "@mui/material"
-
+import { Google } from "@mui/icons-material";
+import { Button, Grid2, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router";
+import { AuthLayout } from "../layout/AuthLayout";
 
 export const LoginPage = () => {
   return (
-    
-    <Grid2
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+    <AuthLayout title="Login">
+      <form>
+        <Grid2 container direction="column">
+          <Grid2 xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid2>
 
-    >
-
-      <Grid2
-        className='box-shadow'
-        xs={3}
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius:2}}>
-        <Typography variant="h5" sx={{mb: 1}}>Login</Typography>
-
-        <form>
-          
-          <Grid2 container direction="column">
-            <Grid2 xs={12} sx={{ mt:2 }}>
-              <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder="correo@google.com" 
-                fullWidth
-                />
-
+          <Grid2 xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="password"
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid2 xs={12}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
             </Grid2>
 
-            <Grid2 xs={12} sx={{ mt:2 }}>
-              <TextField 
-                label="Password" 
-                type="password" 
-                placeholder="password" 
-                fullWidth
-                />
-
+            <Grid2 xs={12}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
             </Grid2>
           </Grid2>
-          
-          
-          </form>      
-      
 
-
-      </Grid2>
-      
-
-    </Grid2>
-  )
-}
+          <Grid2 container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" top="/auth/register">
+              Crear una cuenta
+            </Link>
+          </Grid2>
+        </Grid2>
+      </form>
+    </AuthLayout>
+  );
+};
